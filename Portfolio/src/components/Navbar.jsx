76085dbe870351c00e950c145a5ed09706b07b2f
Jordan Lopez <jordanlopezemail@gmail.com>
2023-10-13 12:04:49 -0700

    fixed navbar and began working on about me page

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,40 +1,39 @@
 import { Link, useLocation } from 'react-router-dom';
 
-export default function Navbar () {
+export default function Navbar() {
 
-    const currentPage = useLocation().pathname;
+  const currentPage = useLocation().pathname;
 
-    return (
-        <div>
-        {(currentPage === '/') ? (
-          <Link to="/" className="linkStyleOne">
-            About Me
-          </Link>
-        ) : (
-          <Link to="/" className="linkStyleTwo">
-            About Me
-          </Link>
-        )}
-  
-        {(currentPage === '/Projects') ? (
-          <Link to="/Projects" className="linkStyleOne">
-            Projects
-          </Link>
-        ) : (
-          <Link to="/Projects" className="linkStyleTwo">
-            Projects
-          </Link>
-        )}
-        {(currentPage === '/Resume') ? (
-          <Link to="/Resume" className="linkStyleOne">
-            Resume
-          </Link>
-        ) : (
-          <Link to="/Resume" className="linkStyleTwo">
-            Resume
-          </Link>
-        )}
-  
-      </div>
-    );
+  return (
+    <div className='navbar'>
+      {(currentPage === '/') ? (
+        <Link to="/" className="linkStyleOne">
+          About Me
+        </Link>
+      ) : (
+        <Link to="/" className="linkStyleTwo">
+          About Me
+        </Link>
+      )}
+
+      {(currentPage === '/Projects') ? (
+        <Link to="/Projects" className="linkStyleOne">
+          Projects
+        </Link>
+      ) : (
+        <Link to="/Projects" className="linkStyleTwo">
+          Projects
+        </Link>
+      )}
+      {(currentPage === '/Resume') ? (
+        <Link to="/Resume" className="linkStyleOne">
+          Resume
+        </Link>
+      ) : (
+        <Link to="/Resume" className="linkStyleTwo">
+          Resume
+        </Link>
+      )}
+    </div>
+  );
 }
--- Portfolio/src/pages/AboutMe.jsx
+++ Portfolio/src/pages/AboutMe.jsx
@@ -1,5 +1,15 @@
 export default function AboutMe () {
     return(
-        <div>About me section</div>
+        <>
+            <p>hello!</p>
+            <h1>I'm Jordan Lopez</h1>
+            <p>A dedicated, adaptable business Consultant and Web Developer with a strong background in Mathematics, Economics, and
+                industry-standard Full Stack technologies.
+                
+                Dedicated and adaptable Consultant and Web Developer with background in Economics, Mathematics, 
+                and industry-standard Full Stack technologies. Eager to enhance proficiency in front/back end development 
+                while contributing meaningfully to outsourced business development.</p>
+            <img id="me" src="/me.png" alt="nothing" />
+        </>
     );
 } 
