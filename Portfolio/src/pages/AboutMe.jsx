0864d47c3b59632534ea8b42a9ebc96f37cf6d54
Jordan Lopez <jordanlopezemail@gmail.com>
2023-10-13 14:32:55 -0700

    about me page is almost done

--- Portfolio/src/pages/AboutMe.jsx
+++ Portfolio/src/pages/AboutMe.jsx
@@ -1,15 +1,18 @@
 export default function AboutMe () {
     return(
         <>
-            <p>hello!</p>
-            <h1>I'm Jordan Lopez</h1>
-            <p>A dedicated, adaptable business Consultant and Web Developer with a strong background in Mathematics, Economics, and
-                industry-standard Full Stack technologies.
-                
-                Dedicated and adaptable Consultant and Web Developer with background in Economics, Mathematics, 
-                and industry-standard Full Stack technologies. Eager to enhance proficiency in front/back end development 
-                while contributing meaningfully to outsourced business development.</p>
-            <img id="me" src="/me.png" alt="nothing" />
+            <div className="introDiv">
+                <p className="spaceBottom">Hello!</p>
+                <h1 id="Jordan">I'm Jordan Lopez</h1>
+                <p>A dedicated, adaptable business Consultant and Web Developer with a strong background in Mathematics, Economics, and
+                    industry-standard Full Stack technologies. An example of my skills would be this website--made with React and vanilla CSS!
+                    If you are interested in my work, click on <span style={{fontWeight: '600'}}>Projects</span> to see all of my 
+                    web-dev abilities or <span style={{fontWeight: '600'}}>Resume</span> to get a complete picture.
+                </p>
+            </div>
+            <div className="pictureDiv">
+                <img id="me" src="/me.png" alt="nothing" />
+            </div>
         </>
     );
 } 
