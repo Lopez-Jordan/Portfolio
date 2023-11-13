8775cb8e01f6aa0b411bedf182dd23e1de3d70ce
Jordan Lopez <jordanlopezemail@gmail.com>
2023-11-13 11:43:01 -0800

    updated styles

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -20,45 +20,43 @@ export default function AboutMe() {
     const [hoveredIcon, setHoveredIcon] = useState(null);
     return (
         <>
-            <div className="bgColorDiv">
-                <div className="introDiv">
-                    <p className="spaceBottom">Hello!</p>
-                    <h1 id="Jordan">I'm Jordan Lopez</h1>
-                    <p className="text">A Software Developer with a strong background in industry-standard Full Stack technologies, Mathematics, Economics, 
+            <div className="introDiv">
+                <p className="spaceBottom">Hello!</p>
+                <h1 id="Jordan">I'm Jordan Lopez</h1>
+                <p className="text">A Software Developer with a strong background in industry-standard Full Stack technologies, Mathematics, Economics,
                     and Consulting. This website was made with React and vanilla CSS--no templates!
-                        If you are interested in my work, click on <span style={{ fontWeight: '600' }}>Projects</span> to see all of my
-                        web-dev abilities or <span style={{ fontWeight: '600' }}>Resume</span> to get a complete picture.
-                    </p>
-                </div>
-                <div className="boxDiv">
-                </div>
-                <div className="pictureDiv">
-                    <div className="icons">
-                        <a className="icon" href="mailto:jordanlopezemail@gmail.com">
-                            <FaEnvelope
-                                style={hoveredIcon === 'Mail' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                                onMouseEnter={() => setHoveredIcon('Mail')}
-                                onMouseLeave={() => setHoveredIcon(null)}
-                            />
-                        </a>
+                    If you are interested in my work, click on <span style={{ fontWeight: '600' }}>Projects</span> to see all of my
+                    web-dev abilities or <span style={{ fontWeight: '600' }}>Resume</span> to get a complete picture.
+                </p>
+            </div>
+            <div className="boxDiv">
+            </div>
+            <div className="pictureDiv">
+                <div className="icons">
+                    <a className="icon" href="mailto:jordanlopezemail@gmail.com">
+                        <FaEnvelope
+                            style={hoveredIcon === 'Mail' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('Mail')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
+                    </a>
 
-                        <a className="icon" href="https://www.linkedin.com/in/jordanlopez123/">
-                            <FaLinkedinIn
-                                style={hoveredIcon === 'LinkedIn' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                                onMouseEnter={() => setHoveredIcon('LinkedIn')}
-                                onMouseLeave={() => setHoveredIcon(null)}
-                            />
-                        </a>
-                        <a className="icon" href="https://github.com/Lopez-Jordan">
-                            <FaGithub
-                                style={hoveredIcon === 'Github' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                                onMouseEnter={() => setHoveredIcon('Github')}
-                                onMouseLeave={() => setHoveredIcon(null)}
-                            />
-                        </a>
-                    </div>
-                    <img id="me" src="/me.png" alt="nothing" />
+                    <a className="icon" href="https://www.linkedin.com/in/jordanlopez123/">
+                        <FaLinkedinIn
+                            style={hoveredIcon === 'LinkedIn' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('LinkedIn')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
+                    </a>
+                    <a className="icon" href="https://github.com/Lopez-Jordan">
+                        <FaGithub
+                            style={hoveredIcon === 'Github' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('Github')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
+                    </a>
                 </div>
+                <img id="me" src="/me.png" alt="nothing" />
             </div>
         </>
     );
