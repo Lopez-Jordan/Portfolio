3cdf752cfda3b4ec2e38b4b18304c021854879c2
Jordan Lopez <jordanlopezemail@gmail.com>
2023-11-13 11:24:39 -0800

    updated styling adn resume

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,50 +2,52 @@ import Card from '../components/Card';
 export default function Projects() {
     return (
         <>
-            <div className="introDiv" id="projectDiv">
-                <h2 id="Jordan">Check out my projects!</h2>
-                <p className='text'>
-                    I've done everything from full stack applications, data base management, financial dashboards,
-                    weather reports and more!
-                </p>
-            </div>
-            <div className='cardContainer'>
-                <Card
-                    name="Surf Report"
-                    deployed="https://floating-shelf-66988-88c62d989e6d.herokuapp.com/"
-                    repo="https://github.com/Lopez-Jordan/SurfReport"
-                    imagePath="/SurfReport.png"
-                />
-                <Card
-                    name="forumZ"
-                    deployed="https://forumz-090c0b5b4bff.herokuapp.com/"
-                    repo="https://github.com/Lopez-Jordan/forumZ"
-                    imagePath="/forumZ.png"
-                />
-                <Card
-                    name="Crypto Portfolio"
-                    deployed="https://ailllycxr.github.io/cryptoDashboard/"
-                    repo="https://github.com/Lopez-Jordan/cryptoDashboard"
-                    imagePath="/crypto.png"
-                />
-                <Card
-                    name="SQL Manager"
-                    deployed="https://github.com/Lopez-Jordan/Employee-Tracker"
-                    repo="https://github.com/Lopez-Jordan/Employee-Tracker"
-                    imagePath="/employeeTracker.png"
-                />
-                <Card
-                    name="Weather Report"
-                    deployed="https://lopez-jordan.github.io/weather-app/"
-                    repo="https://github.com/Lopez-Jordan/weather-app"
-                    imagePath="/weather.png"
-                />
-                <Card
-                    name="JS Quiz"
-                    deployed="https://lopez-jordan.github.io/javascript-quiz/"
-                    repo="https://github.com/Lopez-Jordan/javascript-quiz"
-                    imagePath="/quiz.png"
-                />
+            <div className='bgColorDiv'>
+                <div className="introDiv" id="projectDiv">
+                    <h2 id="Jordan">Check out my projects!</h2>
+                    <p className='text'>
+                        I've done everything from full stack applications, data base management, financial dashboards,
+                        weather reports and more!
+                    </p>
+                </div>
+                <div className='cardContainer'>
+                    <Card
+                        name="Surf Report"
+                        deployed="https://floating-shelf-66988-88c62d989e6d.herokuapp.com/"
+                        repo="https://github.com/Lopez-Jordan/SurfReport"
+                        imagePath="/SurfReport.png"
+                    />
+                    <Card
+                        name="forumZ"
+                        deployed="https://forumz-090c0b5b4bff.herokuapp.com/"
+                        repo="https://github.com/Lopez-Jordan/forumZ"
+                        imagePath="/forumZ.png"
+                    />
+                    <Card
+                        name="Crypto Portfolio"
+                        deployed="https://ailllycxr.github.io/cryptoDashboard/"
+                        repo="https://github.com/Lopez-Jordan/cryptoDashboard"
+                        imagePath="/crypto.png"
+                    />
+                    <Card
+                        name="SQL Manager"
+                        deployed="https://github.com/Lopez-Jordan/Employee-Tracker"
+                        repo="https://github.com/Lopez-Jordan/Employee-Tracker"
+                        imagePath="/employeeTracker.png"
+                    />
+                    <Card
+                        name="Weather Report"
+                        deployed="https://lopez-jordan.github.io/weather-app/"
+                        repo="https://github.com/Lopez-Jordan/weather-app"
+                        imagePath="/weather.png"
+                    />
+                    <Card
+                        name="JS Quiz"
+                        deployed="https://lopez-jordan.github.io/javascript-quiz/"
+                        repo="https://github.com/Lopez-Jordan/javascript-quiz"
+                        imagePath="/quiz.png"
+                    />
+                </div>
             </div>
         </>
     );
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -25,50 +25,53 @@ export default function Resume() {
     const [hoveredIcon, setHoveredIcon] = useState(null);
     return (
         <>
-            <div className="flexDiv">
-                <div className="skillsDiv">
-                    <h2 id="Jordan">Check out my technical skills!</h2>
+            <div className="bgColorDiv">
+
+                <div className="flexDiv">
+                    <div className="skillsDiv">
+                        <h2 id="Jordan">Check out my technical skills!</h2>
+                    </div>
+                    <button className="resume">            
+                        <a
+
+                        href="/JordanLopezResume.pdf"
+                        download="Resume.pdf"
+                        >
+                        Download Resume
+                        </a>
+                    </button>
                 </div>
-                <button className="resume">            
-                    <a
-
-                    href="/JordanLopezResume.pdf"
-                    download="Resume.pdf"
-                    >
-                    Download Resume
+                <div className="boxDiv2">
+                </div>
+                <div id="allSkill">
+                    <SkillCard title="Front End" list={list1} />
+                    <SkillCard title="Back End" list={list2} />
+                    <SkillCard title="Other" list={list3} />
+                </div>
+                <div className="icons2">
+                    <a className="icon" href="mailto:jordanlopezemail@gmail.com">
+                        <FaEnvelope
+                            style={hoveredIcon === 'Mail' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('Mail')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
                     </a>
-                </button>
-            </div>
-            <div className="boxDiv2">
-            </div>
-            <div id="allSkill">
-                <SkillCard title="Front End" list={list1} />
-                <SkillCard title="Back End" list={list2} />
-                <SkillCard title="Other" list={list3} />
-            </div>
-            <div className="icons2">
-                <a className="icon" href="mailto:jordanlopezemail@gmail.com">
-                    <FaEnvelope
-                        style={hoveredIcon === 'Mail' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                        onMouseEnter={() => setHoveredIcon('Mail')}
-                        onMouseLeave={() => setHoveredIcon(null)}
-                    />
-                </a>
 
-                <a className="icon" href="https://www.linkedin.com/in/jordanlopez123/">
-                    <FaLinkedinIn
-                        style={hoveredIcon === 'LinkedIn' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                        onMouseEnter={() => setHoveredIcon('LinkedIn')}
-                        onMouseLeave={() => setHoveredIcon(null)}
-                    />
-                </a>
-                <a className="icon" href="https://github.com/Lopez-Jordan">
-                    <FaGithub
-                        style={hoveredIcon === 'Github' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
-                        onMouseEnter={() => setHoveredIcon('Github')}
-                        onMouseLeave={() => setHoveredIcon(null)}
-                    />
-                </a>
+                    <a className="icon" href="https://www.linkedin.com/in/jordanlopez123/">
+                        <FaLinkedinIn
+                            style={hoveredIcon === 'LinkedIn' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('LinkedIn')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
+                    </a>
+                    <a className="icon" href="https://github.com/Lopez-Jordan">
+                        <FaGithub
+                            style={hoveredIcon === 'Github' ? { ...iconStyle, ...iconHoverStyle } : iconStyle}
+                            onMouseEnter={() => setHoveredIcon('Github')}
+                            onMouseLeave={() => setHoveredIcon(null)}
+                        />
+                    </a>
+                </div>
             </div>
         </>
     )
